b1dbf6a3f2786dce58368b99553f772e6a41881a
Gennady Vasilev <whidrubeld@gmail.com>
2020-06-04 13:29:16 +0300

    app render update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-import { StatusBar } from 'react-native'
-
-import { Text, View, StyleSheet, Button } from 'react-native'
+import {
+  StatusBar,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+} from 'react-native'
 
 import { ViroVRSceneNavigator, ViroARSceneNavigator } from 'react-viro'
 
-import ARScene from './screens/ARScene'
-import VRScene from './screens/VRScene'
+import ARScene from './screens/ar.scene'
+import VRScene from './screens/vr.scene'
 
 const sharedProps = { apiKey: '' }
 
@@ -19,42 +23,46 @@ const App = () => {
   const [navigator, setNavigator] = React.useState(UNSET)
 
   const renderMenu = () => (
-    <View style={styles.outer}>
-      <StatusBar barStyle="light-content" />
-      <View style={styles.inner}>
-        <Text style={styles.title}>Выбор сцены</Text>
+    <View style={styles.inner}>
+      <Text style={styles.title}>Выбор сцены!!</Text>
 
-        <Button
-          title="Дополненная реальность"
-          onPress={() => setNavigator(AR_NAVIGATOR)}
-        />
-        <Button
-          title="Виртуальная реальность"
-          onPress={() => setNavigator(VR_NAVIGATOR)}
-        />
-      </View>
+      <Button
+        title="Дополненная реальность"
+        onPress={() => setNavigator(AR_NAVIGATOR)}
+      />
+      <Button
+        title="Виртуальная реальность"
+        onPress={() => setNavigator(VR_NAVIGATOR)}
+      />
     </View>
   )
 
   const renderVRNavigator = () => (
-    <ViroVRSceneNavigator
-      {...sharedProps}
-      initialScene={{ scene: VRScene }}
-      onExitViro={() => setNavigator(UNSET)}
-    />
+    <ViroVRSceneNavigator {...sharedProps} initialScene={{ scene: VRScene }} />
   )
 
   const renderARNavigator = () => (
     <ViroARSceneNavigator {...sharedProps} initialScene={{ scene: ARScene }} />
   )
 
-  if (navigator == UNSET) {
-    return renderMenu()
-  } else if (navigator == VR_NAVIGATOR) {
-    return renderARNavigator()
-  } else if (navigator == AR_NAVIGATOR) {
-    return renderVRNavigator()
+  const renderScreen = () => {
+    if (navigator == UNSET) {
+      return renderMenu()
+    } else if (navigator == AR_NAVIGATOR) {
+      return renderARNavigator()
+    } else if (navigator == VR_NAVIGATOR) {
+      return renderVRNavigator()
+    }
   }
+
+  const renderBackAction = () => {}
+
+  return (
+    <View style={styles.outer}>
+      <StatusBar barStyle="dark-content" />
+      {renderScreen()}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
